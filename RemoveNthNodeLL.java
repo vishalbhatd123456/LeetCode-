f75451c6e8d10cc881c07913ec3e797a34442725
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-07-11 12:18:26 +0530

    Create RemoveNthNodeLL.java

---
+++ RemoveNthNodeLL.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        
+        ListNode dummy_head = new ListNode(0);
+        
+        
+        dummy_head.next = head;
+        
+        
+        ListNode slow = dummy_head;
+        ListNode fast = dummy_head;
+        
+        
+        for(int i=1;i<=n+1;i++)
+        {
+            fast = fast.next;
+        }
+        
+        
+        while(fast!=null)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        
+        slow.next = slow.next.next;
+        return dummy_head.next;
+        
+        
+    }
+}
